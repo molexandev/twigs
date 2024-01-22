52cafbba783d84cfa2d37bed38564c5e56f6fb25
molexandev01~ <molexandev@gmail.com>
2024-01-22 18:09:32 +0200

    added layout of several blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
-$(function(){
-   
-})
+$(function () {
+   // Слайдер
+
+   $(".best-sellers__slider").slick({
+      infinite: true,
+      arrows: true,
+      dots: false,
+      autoplay: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      // fade: true,
+      // draggable: true,
+   });
+
+   $(".reviews__slider").slick({
+      infinite: true,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      fade: true,
+      // draggable: true,
+   });
+});
