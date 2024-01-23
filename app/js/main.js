565dc4017b7b588b7cb7780a225b2ecb51749ec1
molexandev01~ <molexandev@gmail.com>
2024-01-23 17:29:00 +0200

    added button to up

--- app/js/main.js
+++ app/js/main.js
@@ -39,3 +39,37 @@ $("#menu").on("click", "a", function (e) {
       $("body, html").animate({ scrollTop: top }, 1500);
    }
 });
+
+// Функція для прокрутки сторінки наверх
+function scrollToTop() {
+   // Прокрутити сторінку до верху з плавністю
+   window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+   });
+}
+
+// Функція для прокрутки сторінки наверх
+function scrollToTop() {
+   // Прокрутити сторінку до верху з плавністю
+   window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+   });
+}
+
+// Отримати елемент кнопки
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Додати обробник подій для події прокрутки
+window.addEventListener("scroll", function () {
+   // Визначити, чи користувач почав скролити сторінку
+   if (
+      document.body.scrollTop > 93 ||
+      document.documentElement.scrollTop > 93
+   ) {
+      scrollToTopBtn.style.display = "flex";
+   } else {
+      scrollToTopBtn.style.display = "none";
+   }
+});
