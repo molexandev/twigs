467c5ddb67e8bfe94efa89fa09f8912fdfc84873
molexandev01~ <molexandev@gmail.com>
2024-01-23 17:08:21 +0200

    added pc version

--- app/js/main.js
+++ app/js/main.js
@@ -9,8 +9,6 @@ $(function () {
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplaySpeed: 3000,
-      // fade: true,
-      // draggable: true,
    });
 
    $(".reviews__slider").slick({
@@ -22,6 +20,22 @@ $(function () {
       slidesToScroll: 1,
       autoplaySpeed: 3000,
       fade: true,
-      // draggable: true,
+      draggable: true,
    });
+});
+
+// Плавний скрол
+
+$("#menu").on("click", "a", function (e) {
+   e.preventDefault();
+   var id = $(this).attr("href"),
+      target = $(id),
+      header = $("#menu"), // Замініть на відповідний селектор
+      headerHeight = header.outerHeight(),
+      offset = 120; // Замініть це на бажану величину в пікселях
+
+   if (target.length) {
+      var top = target.offset().top - headerHeight - offset;
+      $("body, html").animate({ scrollTop: top }, 1500);
+   }
 });
