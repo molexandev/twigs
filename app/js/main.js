25be15a688fb5f8484bbb4ac2376dcd27ce94cfd
molexandev01~ <molexandev@gmail.com>
2024-01-25 16:47:59 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,33 @@ $(function () {
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplaySpeed: 3000,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 3,
+            },
+         },
+         {
+            breakpoint: 620,
+            settings: {
+               slidesToShow: 2,
+            },
+         },
+         {
+            breakpoint: 430,
+            settings: {
+               slidesToShow: 1,
+            },
+         },
+         {
+            breakpoint: 380,
+            settings: {
+               slidesToShow: 1,
+               arrows: false,
+            },
+         },
+      ],
    });
 
    $(".reviews__slider").slick({
@@ -29,6 +56,14 @@ $(function () {
       $(".menu__list").toggleClass("active");
       $(".menu__btn").toggleClass("active");
       $("body").toggleClass("lock");
+   });
+
+   // footer slider nav
+
+   $(".footer-nav__title").on("click", function () {
+      // $("footer-nav__list").toggleClass("footer-nav__list--active");
+      $(".footer-nav__title").toggleClass("footer-nav__title--active");
+      $(this).next().slideToggle();
    });
 });
 
